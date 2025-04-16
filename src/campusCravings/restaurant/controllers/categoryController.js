372eacf1eda6cc3f29c95f9a0ad3ed1ba58e649f
Muhammad Usman Zafar <muhammadusmanzafar763@gmail.com>
2025-04-17 00:00:05 +0500

    Implemented restaurant functionalities

---
+++ src/campusCravings/restaurant/controllers/categoryController.js
@@ -0,0 +1,26 @@
+'use strict'
+const restCat = require('../services/categoryService');
+const asyncHandler = require('express-async-handler');
+
+exports.createCategory = asyncHandler(async (req, res) => {
+    const category = await restCat.createCategory(req.body);
+    return category;
+});
+
+exports.getItems = asyncHandler(async (req, res) => {
+    const items = await restCat.getCategoryItemsById(req.params.id);
+    return items;
+});
+
+exports.updateItems = asyncHandler(async (req, res) => {
+    const updateData = req.body;
+
+    const updatedCategory = await restCat.updateCategory(updateData);
+    return updatedCategory;
+});
+
+exports.deleteItems = asyncHandler(async (req, res) => {
+    const itemId = req.params.id;
+    const deletedItem = await restCat.deleteCategoryItem(itemId);
+    return deletedItem;
+});
