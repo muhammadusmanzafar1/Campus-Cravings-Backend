f46eff008f0e09b6e62a7703301b831c8ef1a7c8
Muhammad Usman Zafar <muhammadusmanzafar763@gmail.com>
2025-04-18 01:23:33 +0500

    restaurant registration

--- src/campusCravings/restaurant/models/restaurant.js
+++ src/campusCravings/restaurant/models/restaurant.js
@@ -2,14 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const restaurantSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true
-  },
-  lastName: {
-    type: String,
-    required: true
-  },
+
   storeName: {
     type: String,
     required: true
@@ -19,17 +12,7 @@ const restaurantSchema = new Schema({
     required: true
   },
   floor: String,
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  phone: {
+  phoneNumber: {
     type: String,
     required: true,
     unique: true
@@ -70,9 +53,37 @@ const restaurantSchema = new Schema({
   createdAt: { type: Date, default: Date.now },
   categories: [{
     type: Schema.Types.ObjectId,
-    ref: 'Category', // Reference to the Category model
+    ref: 'Category',
   }],
 });
+
+restaurantSchema.statics.newEntity = async function (body, createdByAdmin = true) {
+  const model = {
+    storeName: body.storeName,
+    brandName: body.brandName,
+    phoneNumber: body.phoneNumber,
+    address: body.address,
+    cuisine: body.cuisine,
+    deliveryMethods: body.deliveryMethods,
+    paymentMethods: body.paymentMethods,
+    floor: body.floor,
+    openingHours: body.openingHours,
+    categories: body.categories || [],
+    userId: body.userId,
+  };
+
+  if (createdByAdmin) {
+    model.status = 'active';
+  } else {
+    model.status = 'pending';
+  }
+
+  return model;
+};
+
+restaurantSchema.statics.isPhoneTaken = async function (phone) {
+  return !!(await this.findOne({ phone }));
+};
 
 const Restaurant = mongoose.model('Restaurant', restaurantSchema);
 
