4ae3413ca546c50f90e5dff403cd9a12d92d43b9
Ghulam Muhammad <95461763+20I-0923@users.noreply.github.com>
2025-04-28 22:42:15 +0500

    Success/Failure events added for frontend

--- src/sockets/events/orderEvents.js
+++ src/sockets/events/orderEvents.js
@@ -4,11 +4,12 @@ module.exports = (io, socket) => {
       {
         socket.join(`order-${orderId}`);
         console.log(`Socket ${socket.id} Joined room order-${orderId}`);
+        socket.emit('order-room-joined', { orderId, message: 'Joined successfully' });
       }
       catch (error)
       {
         console.log(error.message);
-        socket.emit('order-joining-error', error.message);
+        socket.emit('order-joining-error', { error: `Failed to join order room. ${error.message}` });
       }
     });
 };
